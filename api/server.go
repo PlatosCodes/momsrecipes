eaf1d52068e5e882547e973a03cc2775f5d49c74
Alexander <117208524+PlatosCodes@users.noreply.github.com>
2023-07-26 15:10:00 -0400

    fix unchecked validator

--- api/server.go
+++ api/server.go
@@ -32,9 +32,10 @@ func NewServer(config util.Config, store db.Store) (*Server, error) {
 	}
 
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
-		v.RegisterValidation("cuisine_type", validCuisineType)
-	} else {
-		return nil, fmt.Errorf("error binding cusine type validator: %s", err)
+		err = v.RegisterValidation("cuisine_type", validCuisineType)
+		if err != nil {
+			return nil, fmt.Errorf("cannot bind cuisine type validator: %s", err)
+		}
 	}
 
 	server.setupRouter()
